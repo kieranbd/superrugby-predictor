1584a35b6366c139c7bb8a4cb274bf0341e1af46
James-Leslie <jlesl09@gmail.com>
2019-05-02 19:45:58 +1200

    added dash application

---
+++ app.py
@@ -0,0 +1,132 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas as pd
+import numpy as np
+import plotly.graph_objs as go
+
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/kieranbd/superrugby-predictor/master/' +
+    'super_rugby_oddsportal.csv').drop('Play-off Game?', axis=1).dropna()
+
+df['Date'] = pd.to_datetime(df.Date)
+df = df.sort_values('Date', ascending=False)
+df['Year'] = df['Date'].apply(lambda datetime: datetime.year)
+
+df['HomeMargin'] = df['Home Score'] - df['Away Score']
+df['HomeWinProb'] = np.round((1 - df['Home Odds'] / (df['Home Odds'] + df['Away Odds'])),3)
+
+# used for assigning countries to teams
+countries = {'Crusaders': 'NZ',
+             'Chiefs': 'NZ',
+             'Blues': 'NZ',
+             'Hurricanes': 'NZ',
+             'Highlanders': 'NZ',
+             'Bulls': 'SA',
+             'Cheetahs': 'SA',
+             'Kings': 'SA',
+             'Lions': 'SA',
+             'Sharks': 'SA',
+             'Stormers': 'SA',
+             'Brumbies': 'AUS',
+             'Force': 'AUS',
+             'Rebels': 'AUS',
+             'Reds': 'AUS',
+             'Waratahs': 'AUS',
+             'Jaguares': 'ARG',
+             'Sunwolves': 'JPN'}
+
+df['Home Country'] = df['Home Team'].replace(countries)
+df['Away Country'] = df['Away Team'].replace(countries)
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+# initialize Dash
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+colors = {
+    'background': '#000000',
+    'text': '#111111'
+}
+
+intro_text = '''
+## Super Rugby Dashboard
+Work in progress making a super rugby score prediction dashboard. I have been following the dash tutorials found [here](https://dash.plot.ly/)
+'''
+
+app.layout = html.Div(children=[
+
+    dcc.Markdown(children=intro_text),
+
+    dcc.Graph(id='margin-vs-odds'),
+
+    # wrapping slider in Div element allows us to add the 'style' property
+    html.Div([
+        dcc.Slider(
+            id='year-slider',
+            min=df['Year'].min(),
+            max=df['Year'].max(),
+            value=df['Year'].max(),
+            marks={str(year): str(year) for year in df['Year'].unique()},
+            step=None
+        )],
+        style={'marginLeft': '3em',
+               'marginRight': '3em',
+               'marginBottom': '3em'}
+    ),
+
+    dcc.Markdown('#### Last 10 fixtures:'),
+
+    generate_table(df[['Date', 'Home Team', 'Away Team', 'Home Score', 'Away Score', 'HomeWinProb']])
+])
+
+@app.callback(
+    Output('margin-vs-odds', 'figure'),
+    [Input('year-slider', 'value')])
+def update_figure(selected_year):
+    filtered_df = df[df.Year <= selected_year]
+    traces = []
+    for i in filtered_df['Home Country'].unique():
+        df_by_country = filtered_df[filtered_df['Home Country'] == i]
+        traces.append(go.Scatter(
+            x=df[df['Home Country'] == i]['HomeWinProb'],
+            y=df[df['Home Country'] == i]['HomeMargin'],
+            text=df[df['Home Country'] == i]['Home Country'],
+            mode='markers',
+            opacity=0.8,
+            marker={
+                'size': 15,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name=i
+        ))
+
+    return {
+        'data': traces,
+        'layout': go.Layout(
+            xaxis={'title': 'Predicted Home Win Probability'},
+            yaxis={'title': 'Observed Home Score Difference'},
+            margin={'l': 75, 'b': 75, 't': 50, 'r': 20},
+            legend={'x': 1, 'y': 0},
+            hovermode='closest',
+            title='Bookmakers\' odds vs actual scoreline'
+        )
+    }
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ superugby.py
@@ -0,0 +1,193 @@
+import pandas as pd
+import numpy as np
+
+
+def bool_to_int(bool_val):
+    '''used to tally up wins (positive) and losses (negative)'''
+
+    if bool_val:
+        return 1
+    else:
+        return -1
+
+
+def home_streak(df, team, date):
+    '''return consecutive number of wins or losses for home team
+       - if home team won last game, will return the active win streak in all
+         home matches prior to that one
+       - if home team lost last game, will return the active losing streak'''
+
+    # filter only results from before the chosen date
+    date_df = df[df['Date'] < date]
+    # filter only matches for the chosen home team
+    team_df = date_df[date_df['Home_Team']==team].sort_values('Date', ascending=False)
+
+    # count each row backwards in time until streak ends
+    results = team_df.itertuples()
+    try:
+        last_result = bool_to_int(next(results)[-1])
+        streak = last_result
+    except:
+        streak = 0
+
+    while True:
+        try:
+            next_result = bool_to_int(next(results)[-1])
+            if next_result == last_result:
+                streak += next_result
+            else:
+                break
+        except:
+            break
+
+    return streak
+
+
+def away_streak(df, team, date):
+    '''same as home_streak(), but for away team'''
+
+    date_df = df[df['Date'] < date]
+    team_df = date_df[date_df['Away_Team']==team].sort_values('Date', ascending=False)
+    results = team_df.itertuples()
+    try:
+        last_result = bool_to_int(next(results)[-1])*-1
+        streak = last_result
+    except:
+        streak = 0
+
+    while True:
+        try:
+            next_result = bool_to_int(next(results)[-1])*-1
+            if next_result == last_result:
+                streak += next_result
+            else:
+                break
+        except:
+            break
+
+    return streak
+
+
+def home_avg_margin(df, team, date, num_games=5):
+    '''return average home margin by home team in last n games'''
+
+    date_df = df[df['Date'] < date]
+    team_df = date_df[date_df['Home_Team']==team].sort_values('Date', ascending=False)
+
+    # for fixtures at start of the data records return avg home score
+    if len(team_df) == 0:
+        return df['home_margin'].agg('mean')
+
+    team_df = team_df[:num_games]
+
+    home_margin_mean = team_df['home_margin'].agg('mean')
+
+    return home_margin_mean
+
+
+def away_avg_margin(df, team, date, num_games=5):
+    '''return average away margin by away team in last n games'''
+
+    date_df = df[df['Date'] < date]
+    team_df = date_df[date_df['Away_Team']==team].sort_values('Date', ascending=False)
+
+    # for fixtures at start of the data records return avg away score
+    if len(team_df) == 0:
+        return df['home_margin'].agg('mean')*-1
+
+    team_df = team_df[:num_games]
+
+    away_margin_mean = team_df['home_margin'].agg('mean')*-1
+
+    return away_margin_mean
+
+
+def cleanup(op_df):
+    '''
+    performs all necessary data cleaning, processing and feature engineering
+
+    Params:
+        op_df dataframe : dataframe in format as provided by OddsPortal
+    '''
+
+    # used for assigning countries to teams
+    countries = {'Crusaders': 'NZ',
+                 'Chiefs': 'NZ',
+                 'Blues': 'NZ',
+                 'Hurricanes': 'NZ',
+                 'Highlanders': 'NZ',
+                 'Bulls': 'SA',
+                 'Cheetahs': 'SA',
+                 'Kings': 'SA',
+                 'Lions': 'SA',
+                 'Sharks': 'SA',
+                 'Stormers': 'SA',
+                 'Brumbies': 'AUS',
+                 'Force': 'AUS',
+                 'Rebels': 'AUS',
+                 'Reds': 'AUS',
+                 'Waratahs': 'AUS',
+                 'Jaguares': 'ARG',
+                 'Sunwolves': 'JPN'}
+
+    # create copy of odds portal dataframe
+    df = op_df
+
+    # replace spaces in column names with underscores
+    df.columns = df.columns.str.replace(' ', '_')
+
+    # convert Date column to datetime dtype
+    df['Date'] = pd.to_datetime(df.Date)
+
+    # create response variable
+    df['home_margin'] = df['Home_Score'] - df['Away_Score']
+
+    # create home_win column (for use in classification models)
+    df['home_win'] = (df['Home_Score'] > df['Away_Score'])
+
+    # create team form columns
+    home_streaks = []
+    home_avg_margins = []
+    away_streaks = []
+    away_avg_margins = []
+
+    # iterate through each row in df, adding form features for home and away
+    for row in df.itertuples():
+        home_team = row.Home_Team
+        away_team = row.Away_Team
+        date = row.Date
+        home_streaks.append(home_streak(df, home_team, date))
+        home_avg_margins.append(home_avg_margin(df, home_team, date))
+        away_streaks.append(away_streak(df, away_team, date))
+        away_avg_margins.append(away_avg_margin(df, away_team, date))
+
+    df['home_streak'] = home_streaks
+    df['home_avg_marg'] = home_avg_margins
+    df['away_streak'] = away_streaks
+    df['away_avg_marg'] = away_avg_margins
+
+    # add nationalities
+    df['home_nationality'] = df['Home_Team'].replace(countries)
+    df['away_nationality'] = df['Away_Team'].replace(countries)
+
+    # encode nationalities
+    df = pd.get_dummies(df, prefix='home_country', columns=['home_nationality'])
+    df = pd.get_dummies(df, prefix='away_country', columns=['away_nationality'])
+
+    # encode team names
+    df = pd.get_dummies(df, prefix='home_team', columns=['Home_Team'])
+    df = pd.get_dummies(df, prefix='away_team', columns=['Away_Team'])
+
+    # aggregate odds into single probability variable
+    df['home_odds'] = df['Away_Odds'] / (df['Home_Odds'] + df['Away_Odds'])
+
+    # drop irrelevent columns
+    df.drop(['Home_Score', 'Away_Score', 'Home_Odds', 'Draw_Odds', 'Away_Odds',
+             'Bookmakers_Surveyed', 'home_country_ARG', 'home_country_JPN',
+             'away_country_ARG', 'away_country_JPN', 'home_team_Cheetahs',
+             'away_team_Cheetahs', 'home_team_Kings', 'away_team_Kings',
+             'home_team_Force', 'away_team_Force'],
+             axis=1,
+             inplace=True)
+
+    return df
